46ccf27a57eeacb594d697124426f789c449f12a
fredisooon <fedorstrujanov26@gmail.com>
2023-01-21 20:57:55 +0300

    create 'sandbox' app, named - testing

--- regressionApp/urls.py
+++ regressionApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("welcome/", include("welcome.urls"))
+    path("welcome/", include("welcome.urls")),
+    path("testing/", include("testing.urls"))
 ]
